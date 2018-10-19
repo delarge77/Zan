51641207ec7a507ff49496b51b4004198e411f0c
Alessandro Santos Pinto <apinto@cartrawler.com>
2018-10-19 15:37:33 +0100

    onboarding

---
+++ Zan/viewcontrollers/WalkthroughContentViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WalkthroughContentViewController.swift
+//  Zan
+//
+//  Created by Alessandro Santos Pinto on 19/10/2018.
+//  Copyright © 2018 Delarge. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughContentViewController: UIViewController {
+	
+	// MARK: - Outlets
+	
+	
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
---
+++ Zan/viewcontrollers/WalkthroughViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WalkthroughViewController.swift
+//  Zan
+//
+//  Created by Alessandro Santos Pinto on 19/10/2018.
+//  Copyright © 2018 Delarge. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+    }
+    
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+		<#code#>
+	}
+	
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+		<#code#>
+	}
+
+}
